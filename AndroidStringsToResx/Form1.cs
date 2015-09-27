9c2efe3a4f87d493dae6e40eaa8c23ce9b25098f
Anatoly Krivolapov <anatolat@gmail.com>
2015-09-27 22:43:22 +0300

    Added sample project

---
+++ AndroidStringsToResx/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AndroidStringsToResx.Resources;
+
+namespace AndroidStringsToResx
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.Text = Strings.title;
+            textBox2.Text = Strings.escaping_test;
+        }
+
+    }
+}
